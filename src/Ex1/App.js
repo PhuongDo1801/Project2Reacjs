ea8a5c410df5d6768e64f9ee43e4001e621eb4fb
PHUONG.DD194141@sis.hust.edu.vn <PHUONG.DD194141@sis.hust.edu.vn>
2022-06-18 20:04:02 +0700

    rename

--- src/Ex1/App.js
+++ src/Ex1/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-// import ImageList from './ImageList';
-import ImageMatrix from './ImageMatrix';
+import AddData from './AddData';
+import Home from './Home';
+import Navbar from './Navbar';
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from './NotFound';
 function App() {
   return ( 
-  // Bài 2
-  // <ImageList/>
-  <ImageMatrix/>
+    //Bài 1
+  <Router>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path= "/">
+            <Home />
+          </Route>
+          <Route path= "/add">
+            <AddData />
+          </Route>
+          <Route path= "*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  </Router>
   );
 }
 
--- src/Ex2/App.js
+++ src/Ex2/App.js
@@ -1,30 +1,11 @@
 import './App.css';
-import AddData from './AddData';
-import Home from './Home';
-import Navbar from './Navbar';
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NotFound from './NotFound';
+// import ImageList from './ImageList';
+import ImageMatrix from './ImageMatrix';
 function App() {
   return ( 
-    //Bài 1
-  <Router>
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Switch>
-          <Route exact path= "/">
-            <Home />
-          </Route>
-          <Route path= "/add">
-            <AddData />
-          </Route>
-          <Route path= "*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  </Router>
+  // Bài 2
+  // <ImageList/>
+  <ImageMatrix/>
   );
 }
 
